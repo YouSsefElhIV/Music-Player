95ee53c16f966c8fb3101d29295b94f871ab95a9
Youssef El Hayany <161429766+YouSsefElhIV@users.noreply.github.com>
2026-02-09 16:59:14 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,6 @@ import App from './App.jsx'
 import ParticlesTest from './components/particles.jsx'
 
 createRoot(document.getElementById('root')).render(
-    <ParticlesTest />
+    <ParticlesTest />,
     <App />
 )
