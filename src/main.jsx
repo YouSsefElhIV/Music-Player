928bbce62d9bf823b9f45a15c7834b86da37a5ad
Youssef El Hayany <161429766+YouSsefElhIV@users.noreply.github.com>
2026-02-09 15:57:55 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,6 @@ import App from './App.jsx'
 import ParticlesTest from './components/particles.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
     <ParticlesTest />
     <App />
-  </StrictMode>,
 )
