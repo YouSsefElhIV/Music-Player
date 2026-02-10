6eb96d811178dceb25381077208ec5ae00863c75
Youssef El Hayany <161429766+YouSsefElhIV@users.noreply.github.com>
2026-02-10 11:02:03 +0100

    Update particles.jsx

--- src/components/particles.jsx
+++ src/components/particles.jsx
@@ -4,14 +4,10 @@ import { loadSlim } from "tsparticles-slim";
 
 const ParticlesTest = () => {
     const particlesInit = useCallback(async engine => {
-        console.log(engine);
         await loadSlim(engine);
     }, []);
-    const particlesLoaded = useCallback(async container => {
-        await console.log(container);
-    }, []);
     return (
-        <Particles id="tsparticles" url="./particles.json" init={particlesInit} loaded={particlesLoaded} />
+        <Particles id="tsparticles" url="./particles.json" init={particlesInit}/>
     );
 };
 
